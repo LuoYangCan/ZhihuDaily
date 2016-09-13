1673e8a51ef4c204a2e684ba814ade43f6da059a
Young-San Roy <584487926@qq.com>
2016-09-13 09:11:10 +0800

    Initial Commit

---
+++ ZhihuDaily/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZhihuDaily
+//
+//  Created by 孤岛 on 16/9/13.
+//  Copyright © 2016年 孤岛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZhihuDaily/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZhihuDaily
+//
+//  Created by 孤岛 on 16/9/13.
+//  Copyright © 2016年 孤岛. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
